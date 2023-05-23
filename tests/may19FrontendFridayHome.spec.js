e3ea80cf6932ed8bd30e74bf940344ec55e54f30
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-23 11:49:08 -0400

    1 view more details tests written, passin

--- tests/may19FrontendFridayHome.spec.js
+++ tests/may19FrontendFridayHome.spec.js
@@ -11,14 +11,53 @@ test.describe("Homepage", () => {
   }) => {
     await expect(page).toHaveTitle(/Frontend Friday/);
   });
-  // test("has only one h1");
+
+  test("has only one h1", async ({ page }) => {
+    //
+  });
+
   test("has an h1 of 'My TodoList App'", async ({ page }) => {
     await expect(
+      // the 'name' is the text inside of the heading.
       page.getByRole("heading", { name: "My TodoList App" })
     ).toBeVisible();
   });
 });
 
 test.describe("Todo Items", () => {
-  //
+  test("View more details should start with a plus before clicked.", async ({
+    page,
+  }) => {
+    const detailsDivWithPlus = page.getByText("+ View more details", {
+      exact: true,
+    });
+    // The above locator is multiple divs, let's make sure one is visible.
+    const firstDetail = detailsDivWithPlus.first();
+    await expect(firstDetail).toBeVisible();
+    // const secondDetails = detailsDivWithPlus.nth(0);
+    // await expect(secondDetails).toBeVisible();
+  });
+
+  test("View more details should start with a - after being clicked.", async ({
+    page,
+  }) => {
+    // Get locator to first detail.
+    const firstDetail = page.getByText("View more details").first();
+
+    // Before clicking, assert it starts with a +
+    await expect(firstDetail).toHaveText("+ View more details");
+
+    // Click
+    await firstDetail.click();
+
+    // Assert it now starts with a -
+    await expect(firstDetail).toHaveText("- View more details");
+  });
+});
+
+test.describe("Plus Button ", () => {
+  test("Plus button is enabled", async ({ page }) => {
+    const plusButton = page.getByRole("button", { name: "+" });
+    await expect(plusButton).toBeEnabled();
+  });
 });
