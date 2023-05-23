4719af6e5a42233a4f6bdad096205162ed95a082
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-23 10:43:51 -0400

    2 tests written for my frontend friday 5-19 deploy

---
+++ tests/may19FrontendFridayHome.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@playwright/test");
+
+// Go to the home page before each test.
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://frontend-friday-05-19-2023.onrender.com/");
+});
+
+test.describe("Homepage", () => {
+  test("has title containing Frontend Friday", async ({ page }) => {
+    await expect(page).toHaveTitle(/Frontend Friday/);
+  });
+  // test("has only one h1");
+  test("has an h1 of 'My TodoList App'", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", { name: "My TodoList App" })
+    ).toBeVisible();
+  });
+});
+
+test.describe("Todo Items", () => {
+  //
+});
